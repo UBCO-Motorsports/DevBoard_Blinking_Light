2972dd19718a46090941864ecc97fa166ef04050
Lukas Vozenilek <lvozenilek@gmail.com>
2019-11-17 02:18:25 -0800

    Cleaned up code

--- test4/Sources/main.c
+++ test4/Sources/main.c
@@ -43,26 +43,24 @@
 #define SW3 5U
 #define LEDG 15U
 
-  void PORTD_ISR(void) {
-	  //Only run ISR if a single pin has triggered interrupt
-
-	  switch (PINS_DRV_ReadPins(PTD) & (1<<SW2 | 1<<SW3)) {
-	  case 1<<SW2 | 1<<SW3:
-			  //Do nothing if both pressed
-			  PINS_DRV_ClearPortIntFlagCmd(PORTD);
-			  break;
-	  case 1<<SW2:
-			  PINS_DRV_TogglePins(PTD, 1<<LEDR);
-			  PINS_DRV_ClearPinIntFlagCmd(PORTD, SW2);
-			  break;
-	  case 1<<SW3:
-			  PINS_DRV_TogglePins(PTD, 1<<LEDG);
-			  PINS_DRV_ClearPinIntFlagCmd(PORTD, SW3);
-			  break;
-	  }
-
-
+void PORTD_ISR(void) {
+  switch (PINS_DRV_ReadPins(PTD) & (1<<SW2 | 1<<SW3)) {
+  case 1<<SW2 | 1<<SW3:
+		  //Do nothing if both pressed
+		  PINS_DRV_ClearPortIntFlagCmd(PORTD);
+		  break;
+  case 1<<SW2:
+		  PINS_DRV_TogglePins(PTD, 1<<LEDR);
+		  PINS_DRV_ClearPinIntFlagCmd(PORTD, SW2);
+		  break;
+  case 1<<SW3:
+		  PINS_DRV_TogglePins(PTD, 1<<LEDG);
+		  PINS_DRV_ClearPinIntFlagCmd(PORTD, SW3);
+		  break;
   }
+
+
+}
 
 int main(void)
 {
@@ -77,34 +75,22 @@ int main(void)
   /* Write your code here */
   /* For example: for(;;) { } */
 
+    //Setup System Clock
     CLOCK_SYS_Init(g_clockManConfigsArr,CLOCK_MANAGER_CONFIG_CNT,g_clockManCallbacksArr,CLOCK_MANAGER_CALLBACK_CNT);
 	CLOCK_SYS_UpdateConfiguration(0U,CLOCK_MANAGER_POLICY_AGREEMENT);
 
+	//GPIO Setup
     PINS_DRV_Init(NUM_OF_CONFIGURED_PINS, g_pin_mux_InitConfigArr);
     PINS_DRV_SetPinsDirection(PTD, 1<<LEDR | 1<<LEDG );
 
+    //Setup port interrupt
     INT_SYS_InstallHandler(PORT_IRQn, PORTD_ISR, (isr_t*)0);
     INT_SYS_EnableIRQ(PORT_IRQn);
 
-
+    //Endless Loop
     for (;;) {
 
-    	/*
-    	if (PINS_DRV_ReadPins(PTD) & (1<<SW2)) {
-    		PINS_DRV_SetPins(PTD, 1<<LEDR);
-    	} else {
-    		PINS_DRV_ClearPins(PTD, 1<<LEDR);
-    	}
-
-    	if (PINS_DRV_ReadPins(PTD) & (1<<SW3)) {
-    		PINS_DRV_SetPins(PTD, 1<<LEDG);
-    	} else {
-    		PINS_DRV_ClearPins(PTD, 1<<LEDG);
-    	}
-		*/
-
     }
-
 
   /*** Don't write any code pass this line, or it will be deleted during code generation. ***/
   /*** RTOS startup code. Macro PEX_RTOS_START is defined by the RTOS component. DON'T MODIFY THIS CODE!!! ***/
